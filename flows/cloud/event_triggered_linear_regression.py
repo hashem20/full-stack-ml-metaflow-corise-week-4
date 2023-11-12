f13271414a87eb90639579ce91960bf0d08d38a0
hashem <sadeghiye@gmail.com>
2023-11-12 11:06:09 +0000

    commit

--- flows/cloud/event_triggered_linear_regression.py
+++ flows/cloud/event_triggered_linear_regression.py
@@ -2,46 +2,48 @@ from metaflow import FlowSpec, step, card, conda_base, current, Parameter, Flow,
 from metaflow.cards import Markdown, Table, Image, Artifact
 
 URL = "https://outerbounds-datasets.s3.us-west-2.amazonaws.com/taxi/latest.parquet"
-DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
-@project(name="fullstack")
-@trigger(events=['s3'])
-@conda_base(libraries={'pandas': '1.4.2', 'pyarrow': '11.0.0', 'numpy': '1.21.2', 'scikit-learn': '1.1.2'})
+@project(name="champion")
+@trigger(events=["s3"])
+@conda_base(
+    libraries={
+        "pandas": "1.4.2",
+        "pyarrow": "11.0.0",
+        "numpy": "1.22.4",
+        "scikit-learn": "1.1.2",
+    }
+)
 class TaxiFarePrediction(FlowSpec):
-
     data_url = Parameter("data_url", default=URL)
 
     def transform_features(self, df):
-
+        # TODO:
         obviously_bad_data_filters = [
-
-            df.fare_amount > 0,         # fare_amount in US Dollars
-            df.trip_distance <= 100,    # trip_distance in miles
-            df.trip_distance > 0
-
-            # TODO: add some logic to filter out what you decide is bad data!
-            # TIP: Don't spend too much time on this step for this project though, it practice it is a never-ending process.
-
-        ]
-
+                df.fare_amount > 0,  # fare_amount in US Dollars
+                df.trip_distance <= 100,  # trip_distance in miles
+                df.trip_distance > 0
+             ]
         for f in obviously_bad_data_filters:
             df = df[f]
-            
+        # Try to complete tasks 2 and 3 with this function doing nothing like it currently is.
+        # Understand what is happening.
+        # Revisit task 1 and think about what might go in this function.
+
         return df
 
     @step
     def start(self):
-
         import pandas as pd
         from sklearn.model_selection import train_test_split
 
         self.df = self.transform_features(pd.read_parquet(self.data_url))
-
         # NOTE: we are split into training and validation set in the validation step which uses cross_val_score.
         # This is a simple/naive way to do this, and is meant to keep this example simple, to focus learning on deploying Metaflow flows.
-        # In practice, you want split time series data in more sophisticated ways and run backtests. 
+        # In practice, you want split time series data in more sophisticated ways and run backtests.
         self.X = self.df["trip_distance"].values.reshape(-1, 1)
         self.y = self.df["total_amount"].values
+        # self.y = self.df["fare_amount"].values
         self.next(self.linear_model)
 
     @step
@@ -51,19 +53,19 @@ class TaxiFarePrediction(FlowSpec):
 
         # TODO: Play around with the model if you are feeling it.
         self.model = LinearRegression()
-
+        self.model.fit(self.X,self.y)
+        
         self.next(self.validate)
 
     def gather_sibling_flow_run_results(self):
-
         # storage to populate and feed to a Table in a Metaflow card
         rows = []
 
-        # loop through runs of this flow 
+        # loop through runs of this flow
         for run in Flow(self.__class__.__name__):
             if run.id != current.run_id:
                 if run.successful:
-                    icon = "✅" 
+                    icon = "✅"
                     msg = "OK"
                     score = str(run.data.scores.mean())
                 else:
@@ -74,20 +76,39 @@ class TaxiFarePrediction(FlowSpec):
                         for task in step:
                             if not task.successful:
                                 msg = task.stderr
-                row = [Markdown(icon), Artifact(run.id), Artifact(run.created_at.strftime(DATETIME_FORMAT)), Artifact(score), Markdown(msg)]
+                row = [
+                    Markdown(icon),
+                    Artifact(run.id),
+                    Artifact(run.created_at.strftime(DATETIME_FORMAT)),
+                    Artifact(score),
+                    Markdown(msg),
+                ]
                 rows.append(row)
             else:
-                rows.append([Markdown("✅"), Artifact(run.id), Artifact(run.created_at.strftime(DATETIME_FORMAT)), Artifact(str(self.scores.mean())), Markdown("This run...")])
+                rows.append(
+                    [
+                        Markdown("✅"),
+                        Artifact(run.id),
+                        Artifact(run.created_at.strftime(DATETIME_FORMAT)),
+                        Artifact(str(self.scores.mean())),
+                        Markdown("This run..."),
+                    ]
+                )
         return rows
-                
-    
+
     @card(type="corise")
     @step
     def validate(self):
         from sklearn.model_selection import cross_val_score
+
         self.scores = cross_val_score(self.model, self.X, self.y, cv=5)
         current.card.append(Markdown("# Taxi Fare Prediction Results"))
-        current.card.append(Table(self.gather_sibling_flow_run_results(), headers=["Pass/fail", "Run ID", "Created At", "R^2 score", "Stderr"]))
+        current.card.append(
+            Table(
+                self.gather_sibling_flow_run_results(),
+                headers=["Pass/fail", "Run ID", "Created At", "R^2 score", "Stderr"],
+            )
+        )
         self.next(self.end)
 
     @step
